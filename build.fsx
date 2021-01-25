c1aefd6d3e45dd826606adbc1b2e94319bc2c655
Denys Zhuravel <denys.zhuravel@albelli.com>
2021-01-25 16:45:55 +0200

    make gitversion do something

--- build.fsx
+++ build.fsx
@@ -2,16 +2,45 @@
 nuget Fake.DotNet.Cli
 nuget Fake.IO.FileSystem
 nuget Fake.Core.Target
-nuget FSharp.Core 4.7.0 //"
+nuget Fake.Tools.GitVersion 
+nuget FSharp.Core 4.7.0
+
+
+github albumprinter/Fake.Extra
+ //"
 #load ".fake/build.fsx/intellisense.fsx"
+#load ".fake/build.fsx/paket-files/albumprinter/Fake.Extra/GitVersionTool.fs"
+
+open System.IO
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
+open Fake.Tools
+
+open Newtonsoft.Json
+
+open Albelli
+
+    module ScriptVars =
+        let artifacts = "./artifacts" |> Path.GetFullPath
+
+        let version() = GitVersionTool.generateProperties().NuGetVersionV2
+
+        let nugetKey() = "NUGET_API_KEY" |> Environment.environVarOrFail
+
+        let nugetSource() = "https://www.nuget.org/api/v2/package"
+
+
 
 Target.initEnvironment ()
+
+Target.create "Trace" (fun _ ->
+    Trace.log "Hello world"
+    Trace.log <| ScriptVars.version()
+)
 
 Target.create "Clean" (fun _ ->
     !! "src/**/bin"
@@ -27,7 +56,8 @@ Target.create "Build" (fun _ ->
 Target.create "All" ignore
 
 "Clean"
-  ==> "Build"
+  ==> "Trace"
+  //==> "Build"
   ==> "All"
 
 Target.runOrDefault "All"
